e5980c61d1c452a9902e1f9579466ef5bb8e05d8
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-09-02 06:55:38 +0200

    updateLinks: use getAttribute('href') instead of .href to avoid automatically added domains

--- src/updateLinks.js
+++ src/updateLinks.js
@@ -1,6 +1,6 @@
 module.exports = function updateLinks (dom, url) {
   const as = dom.getElementsByTagName('a')
   Array.from(as).forEach(a => {
-    a.href = 'https://' + url + a.href
+    a.setAttribute('href', 'https://' + url + a.getAttribute('href'))
   })
 }
