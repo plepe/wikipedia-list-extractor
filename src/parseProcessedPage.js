b5570c07dbae3f82df0d5bd41c1dc55c903ad597
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-09-09 15:21:33 +0200

    parseProcessedPage: create standalone-function incl. tests

---
+++ src/parseProcessedPage.js
@@ -0,0 +1,125 @@
+const updateLinks = require('./updateLinks.js')
+const wikipediaGetImageProperties = require('./wikipediaGetImageProperties.js')
+
+module.exports = function parseProcessedPage (def, body) {
+  const result = []
+
+  const dom = global.document.createElement('div')
+  dom.innerHTML = body
+
+  const citeRefs = {}
+  if (def.renderedIdInCiteURL) {
+    const reg = new RegExp(def.renderedIdInCiteURL)
+    const cites = dom.querySelectorAll('ol.references > li > span.reference-text > cite > a')
+    Array.from(cites).forEach(a => {
+      const m = a.href.match(reg)
+      if (m) {
+        citeRefs[a.parentNode.parentNode.parentNode.id] = m[1]
+      }
+    })
+  }
+
+  const table = dom.getElementsByClassName(def.renderedTableClass)[0]
+
+  const trs = Array.from(table.rows)
+  trs.splice(0, 1) // ignore header row
+
+  trs.forEach(tr => {
+    let id = tr.id
+
+    if (def.renderedTableRowPrefix) {
+      const m = tr.id.match(new RegExp('^' + def.renderedTableRowPrefix + '(.*)'))
+      if (!m) {
+        return
+      }
+      id = m[1]
+    }
+
+    if (def.renderedIdInCiteURL) {
+      const as = tr.getElementsByTagName('a')
+      Array.from(as).forEach(a => {
+        const cite = a.getAttribute('href').substr(1)
+        if (cite in citeRefs) {
+          id = citeRefs[cite]
+        }
+      })
+
+      if (!id) {
+        return
+      }
+    }
+
+    const item = {}
+
+    Object.keys(def.renderedFields).forEach(fieldId => {
+      const fieldDef = def.renderedFields[fieldId]
+
+      const td = tr.cells[fieldDef.column]
+      if (!td) {
+        return
+      }
+
+      let value
+
+      if (fieldDef.type === 'image') {
+        let imgs = td.getElementsByTagName('img')
+        imgs = Array.from(imgs).filter(img => img.width > 64 && img.height > 64)
+        if (imgs.length) {
+          value = wikipediaGetImageProperties(imgs[0])
+        }
+      } else {
+        updateLinks(td, def.source)
+        let dom = td
+
+        if (fieldDef.domQuery) {
+          dom = dom.querySelector(fieldDef.domQuery)
+          if (!dom) {
+            return
+          }
+        }
+
+        if (fieldDef.domAttribute) {
+          value = dom.getAttribute(fieldDef.domAttribute)
+        } else {
+          value = dom.innerHTML
+        }
+
+        if (fieldDef.replaceOld && fieldDef.replaceNew) {
+          if (!('replaceRegexp' in fieldDef)) {
+            const regexp = fieldDef.replaceOld.match(/^\/(.*)\/(\w*)$/)
+            fieldDef.replaceRegexp = new RegExp(regexp[1], regexp[2])
+          }
+
+          if (!value.match(fieldDef.replaceRegexp)) {
+            return
+          }
+          value = value.replace(fieldDef.replaceRegexp, fieldDef.replaceNew)
+
+          value = value.trim()
+        }
+
+        if (fieldDef.regexp) {
+          if (!fieldDef._regexp) {
+            const regexp = fieldDef.regexp.match(/^\/(.*)\/(\w*)$/)
+            fieldDef._regexp = new RegExp(regexp[1], regexp[2])
+          }
+
+          const m = value.match(fieldDef._regexp)
+          if (!m) {
+            return
+          }
+
+          value = m[1].trim()
+        }
+      }
+
+      if (value) {
+        item[fieldId] = value
+      }
+    })
+
+    result.push(item)
+  })
+
+  return result
+}
---
+++ test/parseProcessedPage.js
@@ -0,0 +1,30 @@
+/* global describe, it */
+require('../node')
+
+const assert = require('assert').strict
+const fs = require('fs')
+
+const parseProcessedPage = require('../src/parseProcessedPage')
+
+const inputs = {
+  Achau: fs.readFileSync('test/data/processed/Achau.html')// .toString()
+}
+
+const expected = {
+  Achau: JSON.parse(fs.readFileSync('test/data/processed/Achau.json'))
+}
+
+const def = JSON.parse(fs.readFileSync('test/def.json'))
+
+describe('parseProcessedPage', () => {
+  it('should return something', () => {
+    const result = parseProcessedPage(def, inputs.Achau)
+    assert.equal(result.length, expected.Achau.length, 'Same amount of items')
+
+    for (let i = 0; i < result.length; i++) {
+      assert.deepEqual(result[i], expected.Achau[i], 'Item ' + i + ' should equal')
+    }
+  })
+})
+
+//    fs.writeFileSync('test.json', JSON.stringify(result, null, '  '))
