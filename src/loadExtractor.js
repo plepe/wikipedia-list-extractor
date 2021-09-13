a0c186d600c4e8ae7e4f1680f6e6a39611de940b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-09-13 17:44:16 +0200

    loadExtractor: optionally override definition via 2nd parameter

--- src/loadExtractor.js
+++ src/loadExtractor.js
@@ -4,8 +4,23 @@ const MediawikiListExtractor = require('./MediawikiListExtractor.js')
 
 const extractors = {}
 
-module.exports = function loadExtractor (id, callback) {
+/**
+ * @param {string} id - id of the extractor (e.g. "AT-BDA")
+ * @param {object} [def] - override definition; if not, load from file
+ * @param {function} callback - return the loaded extractor: (err, extractor)
+ */
+module.exports = function loadExtractor (id, def, callback=null) {
+  if (typeof def === 'function') {
+    callback = def
+    def = null
+  }
+
   if (extractors[id]) {
+    return callback(null, extractors[id])
+  }
+
+  if (def) {
+    extractors[id] = new MediawikiListExtractor(id, def)
     return callback(null, extractors[id])
   }
 
